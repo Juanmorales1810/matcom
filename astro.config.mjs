67776470b25572d51bc38abeea834354f7041090
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2026-06-03 23:32:04 -0300

    feat: agregar integración de sitemap en la configuración de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,14 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://matcom.cl',
   vite: {
       plugins: [tailwindcss()],
-	},
+    },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
